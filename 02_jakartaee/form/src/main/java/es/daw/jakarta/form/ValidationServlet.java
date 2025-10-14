4cde850cad0e7cced945dabeb4d0a02a90327de9
diegomanrique <stopwatch1567@proton.me>
2025-10-14 10:31:40 +0200

    feat: Enhance ValidationServlet and update index.jsp with user input fields and validation

--- 02_jakartaee/form/src/main/java/es/daw/jakarta/form/ValidationServlet.java
+++ 02_jakartaee/form/src/main/java/es/daw/jakarta/form/ValidationServlet.java
@@ -1,4 +1,37 @@
 package es.daw.jakarta.form;
 
-public class ValidationServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.*;
+
+@WebServlet("/ValidationServlet")
+public class ValidationServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Optional<String> nombre = Optional.of(req.getParameter("username"));
+        Optional<String>  password = Optional.of(req.getParameter("password"));
+        Optional<String>  email = Optional.of(req.getParameter("email"));
+        Optional<String> pais = Optional.ofNullable(req.getParameter("pais"));
+        List<String> lenguajes = req.getParameterValues("lenguajes") == null
+                ? List.of()
+                : Arrays.stream(req.getParameterValues("lenguajes")).toList();
+// Still wrong! Same issue.        List<String> roles = List.of(req.getParameterValues("roles"));
+        Optional<String>  idioma = Optional.ofNullable(req.getParameter("idioma"));
+        Optional<String> habilitar = Optional.ofNullable(req.getParameter("habilitar"));
+        Optional<String> secreto = Optional.ofNullable(req.getParameter("secreto"));
+
+        Map<String, String> errores = new HashMap<>();
+
+        if(nombre.get().isBlank()) errores.put("username", "El nombre es " +
+                "obligatorio");
+        if(lenguajes.isBlank()) errores.put("lenguajes","Elige un lenguaje");
+        if(password.isBlank()) errores.put("password","Elige un rol");
+        if(!password.matches("[a-zA-Z")) errores.put("password","tiene que " +
+                "cumplir el patron");
+    }
 }
